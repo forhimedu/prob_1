3d91a1448c04b12204fd9860e169438bbdfb2f3f
forhimedu <160519843+forhimedu@users.noreply.github.com>
2024-04-16 16:54:14 +0500

    Add files via upload

--- arrhenius/script.js
+++ arrhenius/script.js
@@ -14,7 +14,7 @@ function generatePQuestion() {
     const correctAns = (((phosphorus/31)/(2*((100-loss)/100))) * 310 / (percent/100)).toFixed(1);
     const questions = [
         `Қаратау тауларындағы фосфорит кендеріндегі кальций фосфатының \ 
-        массалық үлесі ${percent.toFixed(2).toString()}%. ${phosphorus.toString()}т фосфор алу үшін фосфориттің қандай массасы керек \
+        массалық үлесі ${percent.toFixed(2).toString()}%. ${phosphorus.toFixed(2).toString()}т фосфор алу үшін фосфориттің қандай массасы керек \
         екенін есептеңдер. Өнімнің шығымы ${(100-loss).toFixed(2).toString()}%.`,
         `${phosphorus.toFixed(2).toString()}т фосфор алу үшін құрамында ${percent.toFixed(2).toString()}% кальций фосфаты\
         бар фосфориттің қандай массасы керек екенін есептеңдер. Өндірістегі фосфордың \
@@ -94,7 +94,7 @@ function generateOleum2() {
     const moleSO3 = (Math.random()*3 + parseFloat(moleH2O))
     const moleH2SO4 = (Math.random()*5 + 1)
     const massAcid = (moleH2O*18 + moleH2SO4*98)
-    const massAll = parseFloat(massAcid + moleSO3*80);
+    const massAll = parseFloat(massAcid + moleSO3*80);  
     const percentAcid = (moleH2SO4*98/(parseFloat(massAcid))*100)
     const massSO3 = (moleSO3*80)
     const correctAns = (100*((moleSO3-moleH2O)*80)/massAll).toFixed(1);
@@ -115,12 +115,98 @@ function generateOleum2() {
     }
 }
 
+function generateOleum3() {
+    const moleBoth = Math.random()*5 + 0.1
+    const massSO3 = moleBoth*80;
+    const massH2SO4 = moleBoth*98;
+    const moleH2O = moleBoth + Math.random()*5;
+    const massAll = moleH2O*18 + massSO3 + massH2SO4;
+    const massFinalH2SO4 = massH2SO4*2;
+    const percentH2SO4 = 100*massFinalH2SO4/massAll
+    const correctAns = (moleBoth*(80+98)).toFixed(1);
+    
+    const questionText = 
+    `Күкірт қышқылының ${percentH2SO4.toFixed(2)}%-дық ${massAll.toFixed(2)}г ерітіндісін дайындау үшін суға қосылатын олеум (Н\u2082SO\u2084·SO\u2083) массасын есептеңдер.`
+    
+    const variantsArray = [];
+    for (let i = 0; i<3; i++) {
+        variantsArray.push(generateRandomAnswer(correctAns, correctAns/3, correctAns+100));
+    }
+        
+    return {
+        question: questionText,
+        answer: correctAns,
+        variants: variantsArray,
+    }
+}
 
+function generateOleum4() {
+    const moleSO3 = Math.random()*3;
+    const moleH2SO4 = Math.random()*7;
+    const massSO3 = moleSO3*80;
+    const massH2SO4 = moleH2SO4*98;
+    const percentSO3 = 100*massSO3/(massSO3+massH2SO4).toFixed(2);
+    const moleH2O = Math.random()*10+10+moleSO3;
+    const massH2O = moleH2O*18;
+    const percentFinalH2SO4 = 100*(massH2SO4+moleSO3*98)/(massSO3+massH2SO4+massH2O);
+    const correctAns = (massSO3+massH2SO4).toFixed(1);
+        
+    const questionText = 
+    `
+    ${percentFinalH2SO4.toFixed(2)}%-дық күкірт қышқылының ${(massSO3+massH2SO4+massH2O).toFixed(2)}г ерітіндісін дайындау үшін қанша ${percentSO3.toFixed(2)}%-дық олеумді суда еріту қажет? 
+    `
+    
+    const variantsArray = [];
+    for (let i = 0; i<3; i++) {
+        variantsArray.push(generateRandomAnswer(correctAns, correctAns/3, correctAns+100));
+    }
+        
+    return {
+        question: questionText,
+        answer: correctAns,
+        variants: variantsArray,
+    }
+}
+
+
+//generates proper questions with close answers
+function generateOleum5() { 
+    const massOleum = (100 + parseInt(Math.random()*100) * 10);
+    const percentSO3 = parseInt((Math.random()*90).toFixed());
+    const moleH2O = parseFloat((Math.random()*10 + 2 + percentSO3*massOleum/8000).toFixed(1))
+    const percentH2O = parseInt((Math.random()*90).toFixed());
+    const massSolution = parseFloat(((100*moleH2O*18)/percentH2O).toFixed(2));
+    const mass1H2SO4 = (100-percentSO3)*massOleum/100;
+    const mass2H2SO4 = (100-percentH2O)*massSolution/100;
+    const massNewH2SO4 = (massOleum-mass1H2SO4)*98/80;
+    const massAllH2SO4 = mass1H2SO4 + mass2H2SO4 + massNewH2SO4;
+    const massAll=massOleum + massSolution;
+    
+    const correctAns = parseFloat(( 100*massAllH2SO4/massAll ).toFixed(2));
+        
+    const questionText = 
+    `
+    Массасы ${massOleum}г ${percentSO3}%-дық олеумді \
+    ${massSolution}г  ${(100-percentH2O)}%-дық күкірт  \
+    қышқылында еріткенде алынған күкірт қышқылының массалық үлесін есептеңдер.
+    `
+    
+    const variantsArray = [];
+    for (let i = 0; i<3; i++) {
+        variantsArray.push(generateRandomAnswer(correctAns, correctAns/3, 100));
+    }
+        
+    return {
+        question: questionText,
+        answer: correctAns,
+        variants: variantsArray,
+    }
+}
 
 
 
 //array of question generation functions
-const generateRandomQuestion = [generateNQuestion, generatePQuestion, generateOleum1, generateOleum2];
+const generateRandomQuestion = [generateNQuestion, generatePQuestion, generateOleum1, generateOleum2, generateOleum3, generateOleum4, generateOleum5];
 
 
 //generates the random answer based on given answer, the range is provided as well, 
@@ -159,6 +245,7 @@ function App() {
 
     // generates a question object with its correct answer
     const data = pickRandomElem(generateRandomQuestion);
+    // const data = generateRandomQuestion[6]();
     const questionText = document.createElement('h4');
     questionText.textContent = data.question;
     questionContainer.appendChild(questionText);
